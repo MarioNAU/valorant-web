c09a914a44bbf88a4b1757e9504317b3fd92144c
Mario Angelino Uribe <Mario_Angelino@hotmail.com>
2024-05-25 03:11:19 -0600

    Agents avance

--- src/pages/Agents.tsx
+++ src/pages/Agents.tsx
@@ -1,52 +1,179 @@
 import {useEffect, useState}from "react";
-import { useAgents } from "../context/Datacontext"; "../context/Datacontext";
+import { useAgents } from "../context/Datacontext";
+import MenuBar from "../componentes/MenuBar";
+import styled from 'styled-components';
 
-
+interface Props{
+    primero: string,
+    segundo: string,
+    tercero: string,
+    cuarto: string
+}
 
 function Agents () {
-
     const visibleIconsMinimum = 12;
-    const [startIndex, setStartIndex] = useState(0);
-    
     const { getAgentsData, agents } = useAgents();
+    const [startIndex, setStartIndex] = useState(0);
+    const [idAgent, setIdAgent] = useState(0);
+    const [toggleState, setToggleState] = useState(0);
+
     useEffect(() => {
-        getAgentsData()
+        getAgentsData()    
     }, [])
 
+    const getStyles = (index: number) => {
+        return {
+          primero: agents[index]?.backgroundGradientColors[0],
+          segundo: agents[index]?.backgroundGradientColors[1],
+          tercero: agents[index]?.backgroundGradientColors[2],
+          cuarto: agents[index]?.backgroundGradientColors[3]
+        };
+      };
+
     const next = () => {
-        if(startIndex + visibleIconsMinimum < agents.length){
-            setStartIndex((prevIndex) => prevIndex + 1);
+        if(idAgent < agents.length - 1){
+            setIdAgent(prevIndex => prevIndex + 1);
+            if(idAgent >= startIndex + visibleIconsMinimum -1){
+                setStartIndex(prevIndex => prevIndex + 1);
+            }
         }
     };
 
-    const handlePrev = () => {
-        if(startIndex > 0){
-            setStartIndex(prevIndex => prevIndex - 1);
+    const Prev = () => {
+        if(idAgent > 0){
+            setIdAgent(prevIndex => prevIndex - 1);
+            if(idAgent <= startIndex){
+                setStartIndex(prevIndex => prevIndex - 1);
+            }
         }
     }
 
+    const handleThumbnailClick = (index: number) => {
+        setIdAgent(index);
+    }
+
+    const toggleTab = (index : number) => {
+        setToggleState(index);
+    }
+
     const visibleIcons = agents.slice(startIndex, startIndex + visibleIconsMinimum);
+    const selectedPhoto = agents[idAgent];
 
+    const styles = getStyles(idAgent);
+
+    const AgentsMain= styled.div<Props>`
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background-image: linear-gradient(to right , #${(props) => props.primero} 18%, #${(props) => props.segundo});
+        z-index: 0;
+        filter: brightness(90%);
+            &::after {
+                content: "";
+                left: 0;
+                display: flex;
+                width: 100%;
+                height: 100%;
+                background-image: linear-gradient(to right, #${(props) => props.tercero}, #${(props) => props.cuarto});
+                -webkit-mask: linear-gradient(to bottom,#0000, #000);
+                        mask: linear-gradient(to bottom,#0000, #000);
+                filter: brightness(90%);
+              }
+    `;
+   
+    
     return (
-        <div className="play-main">
-            <div className="roster">
-            {
-                visibleIcons.map((agent)=> (
-                    <div className="agentIcon-container" key={agent.uuid}>
-                        <img className="agentIcon" src={agent.displayIcon}></img>
-                    </div> 
-                ))
-            }
-            </div>
-            <div className="pagination-container">
-                <div className="pagination"><p> &lt; </p></div>
-                <div className="numeros"> {startIndex} / 24 </div>
-                <div className="pagination"><p> &gt; </p></div>
-            </div>
-            
-
         
-        </div>
+        
+        <AgentsMain primero={styles.primero} segundo={styles.segundo} tercero={styles.tercero} cuarto={styles.cuarto}>
+            <MenuBar/>
+            <div className="fullPortrait">
+            <img className="fullicon" src={selectedPhoto?.fullPortrait} />
+                <div className="nameStyle" style={{backgroundImage: `url(${selectedPhoto?.background})`}}/>
+            </div>
+        
+
+            <div className="roster-container">
+                <div className="roster">
+                {
+                    visibleIcons.map((agent, index)=> (
+                        <div className="agentIcon-container" key={agent.uuid}>
+                            <img className="agentIcon" src={agent.displayIcon} onClick={()=>handleThumbnailClick(startIndex+index)}/>
+                        </div> 
+                    ))
+
+                    
+                }
+                </div>
+                <div className="pagination-container">
+                    <button className="pagination" onClick={Prev} disabled={idAgent === 0}>&lt;</button>
+                    <div className="numeros"> {idAgent} / 24 </div>
+                    <button className="pagination" onClick={next} disabled={idAgent === agents.length - 1}> &gt; </button>
+                </div>
+            </div>
+          
+            <div className="container-tabs">
+                <h3 className="rol">INITIATOR</h3>
+                <h1 className="agent-name">GEKKO</h1>
+
+                <div className="bloc-tabs">
+                    <div className={toggleState === 0 ? "tab active-habilitie": "tab"} onClick={() => toggleTab(0)}>
+                        <div className="habilidades">
+                            <div className="habilidad"> INFO </div>
+                            <img className="habilitieIcon" src={selectedPhoto?.role.displayIcon}/>
+                        </div>
+                    </div>
+
+                    <div className="tab" onClick={() => toggleTab(1)}>
+                        <div className="habilidades">
+                            <div className="habilidad">C</div>
+                            <img className="habilitieIcon" src={selectedPhoto?.abilities[0].displayIcon}/>
+                        </div>
+                    </div>
+
+                    <div className="tab" onClick={() => toggleTab(2)}>
+                        <div className="habilidades">
+                            <div className="habilidad">Q</div>
+                            <img className="habilitieIcon" src={selectedPhoto?.abilities[1].displayIcon}/>
+                        </div>
+                    </div>
+
+                    <div className="tab" onClick={() => toggleTab(3)}>
+                        <div className="habilidades">
+                            <div className="habilidad">E</div>
+                            <img className="habilitieIcon" src={selectedPhoto?.abilities[2].displayIcon}/>
+                        </div>
+                    </div>
+
+                    <div className="tab" onClick={() => toggleTab(4)}>
+                        <div className="habilidades">
+                            <div className="habilidad">X</div>
+                             <img className="habilitieIcon" src={selectedPhoto?.abilities[3].displayIcon}/>
+                        </div>
+                       </div>
+
+                </div>
+
+                <div className="content-data">
+                    <div className="content active-content">
+                        <p className="agent-background">{selectedPhoto?.description}</p>
+                        <h3 className="rol-content">{selectedPhoto?.role.displayName}</h3>
+                        <h3>{selectedPhoto?.role.description}</h3>
+                    </div>
+                    <div className="content">
+                        habilidaded Q
+                    </div>
+                    <div className="content">
+                        habilidad 3
+                    </div>
+                    <div className="content">
+                        habilidaded 4
+                    </div>
+                </div>
+            </div>
+        </AgentsMain>
     )
 }
 
--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -1,32 +1,11 @@
-import {IconHome, IconAgents, IconBattleP, Iconbackpack, IconMedal, IconStore} from '../assets/index';
 import reticula from "../assets/objetivo.png";
-import Tooltip from '../componentes/Tooltips';
 import Tabs from '../componentes/Tabs';
+import MenuBar from '../componentes/MenuBar';
 
 function Play () {
     return (
         <div className="play-main">
-            <div className="bar-nav">
-            <Tooltip texto="HOME">
-                <a className='item-nav' href="/"><img src={IconHome} className="icons" /></a>
-            </Tooltip>
-            <Tooltip texto="BATTLEPASS">    
-                <a className='item-nav' href="#"><img src={IconBattleP} className="icons"/></a>
-            </Tooltip>
-            <Tooltip texto="AGENTS">  
-                <a className='item-nav'><img src={IconAgents} className="icons"/></a>
-            </Tooltip>
-                <a className='item-nav'><div className='big-red'>Jugar</div></a>
-            <Tooltip texto="CAREER">  
-                <a className='item-nav'><img src={IconMedal} className="icons"/></a>
-            </Tooltip>
-            <Tooltip texto="COLLECTION">  
-                <a className='item-nav'><img src={Iconbackpack} className="icons"/></a>
-            </Tooltip>
-            <Tooltip texto="STORE">  
-                <a className='item-nav'><img src={IconStore} className="icons"/></a>
-            </Tooltip>
-            </div>
+            <MenuBar/>
 
             <div className='modes-container'>
                 <Tabs titles={['NORMAL', 'COMPETITIVO', 'SWIFTPLAY', 'SPIKERUSH', 'DEATHMATCH', 'CARRERA DE ARMAS', 'TEAM DEATHMATCH']}/>
